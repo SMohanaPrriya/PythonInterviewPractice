9ece5758c57ab4e7acde30a9fce34887071ca2bb
SMohanaPrriya <s.mohanaprriya@gmail.com>
2024-03-03 21:15:02 -0500

    update anagram

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -3,21 +3,19 @@ Input: Two strings s and t
 Output: True, if t is an anagram of s
         False, otherwise
 """
-
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if len(s) == len(t):
+            count_s = {}
+            for i in range(len(s)):
+                count_s[s[i]] = 1 + count_s.get(s[i], 0)
+                count_s[t[i]] = count_s.get(t[i], 0) - 1
+            for key in count_s:
+                if count_s[key] != 0:
+                    return False
+            return True
+        else:
             return False
-        count_s = {}
-        for i in range(len(s)):
-            count_s[s[i]] = 1 + count_s.get(s[i], 0)
-            count_s[t[i]] = count_s.get(t[i], 0) - 1
-
-        for key in count_s:
-            if count_s[key] != 0:
-                return False
-        return True
 
 
 s = Solution()
